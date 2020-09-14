63a92793b9176e1c5ae453fafa73ade83ad51be1
Fib88 <nsukifrank@hotmail.com>
2020-09-14 15:42:44 +0200

    now requesting data on input

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+
+
+
 function getPokemon(){
-    fetch("https://pokeapi.co/api/v2/pokemon/7/")
+    const input = document.getElementById("input").value;
+
+
+    fetch("https://pokeapi.co/api/v2/pokemon/"+input)
         .then(response => response.json())
         .then(data => {
             console.log(data);
             let pokeDisplay = document.getElementById("target");
             pokeDisplay.innerHTML = data.name;
+
+
         })
 }
 getPokemon();
