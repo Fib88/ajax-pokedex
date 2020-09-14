7fe197b1144f85c4e399226bdc22af3f5fe15fa7
Fib88 <nsukifrank@hotmail.com>
2020-09-14 15:09:02 +0200

    getting squirtle to show  on page

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function getPokemon(){
     fetch("https://pokeapi.co/api/v2/pokemon/7/")
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data);
+            let pokeDisplay = document.getElementById("target");
+            pokeDisplay.innerHTML = data.name;
+        })
 }
 getPokemon();
 
- document.getElementById("")
+
+
