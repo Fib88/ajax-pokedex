ca9c40e7c638df41d8d8223d6dd72d5c8de044eb
Fib88 <nsukifrank@hotmail.com>
2020-09-14 14:46:05 +0200

    started over with clearer code

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-function start(){
-
-
-
-    function retrievePokémon() {
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=1050")
-            .then(response => response.json())
-            .then (function(allpokemon){
-            allpokemon.results.forEach(function(pokemon){
-                fetchPokemonData(pokemon);
-            })
-        })
-
-    }
-    retrievePokémon();
-}
-function fetchPokemonData() {
-    let url = "https://pokeapi.co/api/v2/pokemon/1/";
-    fetch("https://pokeapi.co/api/v2/pokemon/1/")
+function getPokemon(){
+    fetch("https://pokeapi.co/api/v2/pokemon/7/")
         .then(response => response.json())
-        .then(function(pokeData){
-            console.log(pokeData)
-        })
+        .then(data => console.log(data))
 }
+getPokemon();
 
-function renderPokemon(pokedata){
-    let allPokemonContainer = document.getElementById("poke-container");
-    let pokeContainer = document.createElement("div");
-    let pokeName = document.createElement('h4');
-    pokeName.innerText = pokeData.name;
-    let pokeNumber = document.createElement('p')
-}
-start()
+ document.getElementById("")
