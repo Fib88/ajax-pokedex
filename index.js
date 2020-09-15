27d33baf0127ae0ec5ba840449c5e1b3e918a7e1
Fib88 <nsukifrank@hotmail.com>
2020-09-15 12:27:19 +0200

    save state before break

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 function getPokemon(){
     const input = document.getElementById("input").value;
 
-
+    //fetch api data based on input
     fetch("https://pokeapi.co/api/v2/pokemon/"+input)
         .then(response => response.json())
         .then(data => {
@@ -12,20 +12,24 @@ function getPokemon(){
             let pokeDisplay = document.getElementById("target");
             pokeDisplay.innerHTML = data.name;
 
+            //log sprites and id to DOM
             document.getElementById("pokeshow").setAttribute("src", `${data.sprites.front_shiny}`)
             document.getElementById("pokeid").innerHTML = data.id;
+
+
+            //log randomised moveset to DOM
             let maxMoves = data.moves.length;
             let randomMove = Math.floor(Math.random()*(maxMoves-1)+1);
-            document.getElementById("moveSet").innerHTML = data.moves[randomMove].move.name;
+            document.getElementById("moveSet").innerHTML = (data.moves[randomMove].move.name)
+
             console.log(data.moves.length);
         })
 }
 
+function multipleRandom(){
+    let moveList = []
+    for(let i=0;i<4;i++){
+        document.getElementById("moveSet").innerHTML = (data.moves[randomMove].move.name)
 
-    // function randomMoves(){
-    //     let maxMoves = data.moves.length;
-    //
-    //     let randomMove = Math.floor(Math.random()*(maxMoves - 1)+1)
-    //     console.log(randomMove);
-    //       return randomMove;
-    // }
+    }
+}
